3fbb022285b525b14a72f895e0cc757f411a74c2
Mir Sahib <mirsahib24@gmail.com>
2023-08-12 10:17:46 +0600

    :hammer: Add category

--- src/container/ProductContainer.tsx
+++ src/container/ProductContainer.tsx
@@ -1,28 +1,40 @@
-
-"use client"
-import { ProductCardProps } from '@/interfaces/interfaces';
-import ProductCard from '@/components/ProductCard';
-import useGetProductData from '@/hooks/useGetProductData';
+"use client";
+import { ProductCardProps } from "@/interfaces/interfaces";
+import ProductCard from "@/components/ProductCard";
+import useGetProductData from "@/hooks/useGetProductData";
+import useGetProductCategory from "@/hooks/useGetProductCatagory";
+import { Button } from "@/components/ui/button";
+import truncateText from "@/lib/util/truncateText";
 
 const products: ProductCardProps[] = [
     {
-      product_id: 1,
-      product_image: '/website-2.0-header.png',
-      product_title: 'Product 1',
-      product_description:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam hic saepe totam voluptates impedit exercitationem eligendi placeat id nihil debitis!',
-      product_price: 9.0,
-      product_shelf_life: '2021-10-10',
+        product_id: 1,
+        product_image: "/website-2.0-header.png",
+        product_title: "Product 1",
+        product_description:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam hic saepe totam voluptates impedit exercitationem eligendi placeat id nihil debitis!",
+        product_price: 9.0,
+        product_shelf_life: "2021-10-10",
     },
-  ];
-  
+];
+
 export default function ProductContainer() {
-    const {products,loading} = useGetProductData()
-  return (
-    <div className='grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-3'>
-        {products && products.map((product) => (
-          <ProductCard key={product.id} {...product} />
-        ))}
-      </div>
-  )
+    const { catagory, loadingCategory } = useGetProductCategory();
+    const { products, loading } = useGetProductData();
+    return (
+        <div className="">
+            <div className="flex gap-4 mb-5">
+              {catagory && catagory.length > 0 && (
+                catagory.map(item=><Button title={item} variant={'secondary'} >{truncateText(item,16)}</Button>)
+              )}
+            </div>
+
+            <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-3">
+                {products &&
+                    products.map((product) => (
+                        <ProductCard key={product.id} {...product} />
+                    ))}
+            </div>
+        </div>
+    );
 }
